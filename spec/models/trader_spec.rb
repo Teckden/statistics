e27d4e6b447f403cf8614df2aa92d8e6eed2d702
Teckden <denys.ivanchuk@gmail.com>
2014-01-04 15:04:03 +0200

    Refactored tests using 'shoulda-matchers' gem

--- spec/models/trader_spec.rb
+++ spec/models/trader_spec.rb
@@ -1,15 +1,11 @@
 require 'spec_helper'
 
 describe Trader do
+
+  it { should have_many(:weekly_summaries) }
+
   context 'when valid' do
     it { expect(FactoryGirl.build(:trader)).to be_valid }
-  end
-
-  context 'when has many weekly summaries' do
-    it 'should have association with weekly_summaries' do
-      t = Trader.reflect_on_association(:weekly_summaries)
-      expect(t.macro).to be(:has_many)
-    end
   end
 
 end
--- spec/models/weekly_summary_spec.rb
+++ spec/models/weekly_summary_spec.rb
@@ -2,17 +2,12 @@ require 'spec_helper'
 
 describe WeeklySummary do
 
+  it { should belong_to(:trader) }
+
   describe "WeeklySummary record" do
 
     context 'when valid' do
       it { expect(FactoryGirl.build(:weekly_summary)).to be_valid }
-    end
-
-    context 'when belongs_to trader' do
-      it 'should belongs_to trader' do
-        ws = WeeklySummary.reflect_on_association(:trader)
-        expect(ws.macro).to be(:belongs_to)
-      end
     end
 
   end
