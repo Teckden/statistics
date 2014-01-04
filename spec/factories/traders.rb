7a9719a0509489132871ecebb60ae7116e91d1b2
Teckden <denys.ivanchuk@gmail.com>
2014-01-04 15:57:28 +0200

    Added factory with association

--- spec/factories/traders.rb
+++ spec/factories/traders.rb
@@ -1,10 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
   factory :trader do
     pamm_name "john"
     pamm_url "URL"
     pamm_broker "happy"
     pamm_num 12345
+
+    factory :trader_with_weekly_summary do
+      after(:create) do |trader|
+        create_list(:weekly_summary, 2, trader: trader)
+      end
+    end
   end
+
 end
--- spec/models/trader_spec.rb
+++ spec/models/trader_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Trader do
 
-  it { should have_many(:weekly_summaries) }
+  it { should have_many(:weekly_summaries).dependent(:destroy) }
 
   context 'when valid' do
     it { expect(FactoryGirl.build(:trader)).to be_valid }
